cbedf48f4e5ebe1dc4545c3d3e5d6bf75e800d14
Kazuaki Masuda <kame.kazu.0213.masumerry.0112@gmail.com>
2016-07-25 18:31:21 +0900

    change a little

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,22 @@
 var gulp = require('gulp');
 var webpack = require('gulp-webpack');
 var webpackConfig = require('./webpack.config.js');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
 gulp.task('build', function (cb) {
   return gulp.src('app/assets/build.js')
-  .pipe(webpack(webpackConfig))
-  .pipe(gulp.dest(''));
+    .pipe(webpack(webpackConfig))
+    .pipe(gulp.dest(''));
+});
+
+gulp.task('serve', function () {
+  browserSync({
+    proxy: {
+      target: 'http://localhost:3000'  
+    },
+    port: 8080
+  });
+
+  gulp.watch(['./frontend/**/*.js', './frontend/**/*.scss'], ['build', reload]);
 });
